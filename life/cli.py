f6be65e298b0ce936b1ad8973a26f47d0f61a802
Zirubihara <krystian.swiecicki@boldare.com>
2023-12-03 00:58:09 +0100

    working on cli arguments

--- life/cli.py
+++ life/cli.py
@@ -8,3 +8,15 @@ def get_command_line_args():
         prog="life",
         description="Conway's Game of Life",
     )
+
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--pattern",
+        choices=[p.name for p in patterns.get_all_patterns],
+        default="glider",
+        help="Pattern to start with",
+    )
